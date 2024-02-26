401836e6a8a3d732ee3d740b6e5a854a3e82a2f5
Jason Lo <lcmjlo@gmail.com>
2024-02-26 16:03:01 -0600

    update chtc job to use redis queue worker

--- chtc/job.sh
+++ chtc/job.sh
@@ -1,31 +1,25 @@
+source .env
+
 echo "Running job on `hostname`"
 echo "GPUs assigned: $CUDA_VISIBLE_DEVICES"
+echo "Queue: redis://$REDIS_HOST:$REDIS_PORT"
 
-# export HOME=$_CONDOR_SCRATCH_DIR
-# export LD_SO_CACHE=$_CONDOR_SCRATCH_DIR/ld_so_cache
-export SQLITE_DB_PATH="/staging/clo36/text2graph/data/eval.db"
-export INPUT_FILE="/staging/clo36/text2graph/data/formation_sample.parquet.gzip"
+export HOME=$_CONDOR_SCRATCH_DIR
 
-echo "SQLITE_DB_PATH: $SQLITE_DB_PATH"
-echo "INPUT_FILE: $INPUT_FILE"
+echo "Copy ollama cache folder from staging to scratch dir"
+# Do not use symlink, very slow and unstable.
+cp -r /staging/clo36/.ollama ~/.ollama
 
-echo "Symlinking ollama cache folder to staging"
-ln -s /staging/clo36/.ollama ~/.ollama
-
-echo "Starting ollama"
+echo "Starting ollama..."
 ollama serve &
 
 echo "Waiting for ollama to start"
-sleep 15
-
-echo "Building custom ollama"
-ollama create custom_mixtral -f ./Modelfile
+sleep 10
 
 # Warm up
-echo "Warming up custom_ollama"
-ollama run custom_mixtral "this is a warm up query"
+echo "Warming up ollama"
+ollama run mixtral "this is a warm up query"
 
-curl http://localhost:11434/api/generate -d '{"model": "custom_mixtral", "keep_alive": -1}'
-
-# Run the job
-# python job.py $1 $2 $3 $4
+# Run the worker with burst mode
+echo "Running Redis Queue worker..."
+rq worker --burst --url redis://$REDIS_HOST:$REDIS_PORT
