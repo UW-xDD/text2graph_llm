e8daa14e4fe123423e8c94d2b5bd09fb50970b98
Jason Lo <lcmjlo@gmail.com>
2024-03-14 17:12:55 +0000

    get real entity list from macrostrat

--- demo/macrostrat.py
+++ demo/macrostrat.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-from text2graph.macrostrat import find_all_occurrences
+from text2graph.macrostrat import find_all_occurrences, get_known_entities
 
-TEST_ANNOTATIONS = {
-    "Everton": "strat name",
-    "Shakopee": "strat name",
-}
 
 ANNOTATION_TEMPLATE = """<a href=\"{url}\"><span style=\"display: inline-flex; flex-direction: row; align-items: center; background: rgba(128, 132, 149, 0.4); border-radius: 0.5rem; padding: 0.25rem 0.5rem; overflow: hidden; line-height: 1;\">{word}<span style=\"border-left: 1px solid; opacity: 0.1; margin-left: 0.5rem; align-self: stretch;\"></span><span style=\"margin-left: 0.5rem; font-size: 0.75rem; opacity: 0.5;\">{label}</span></span></a>"""
 
@@ -14,8 +10,8 @@ def get_known_term_html(word: str, label: str, url: str) -> str:
     return ANNOTATION_TEMPLATE.format(word=word, label=label, url=url)
 
 
-def inject_annotations(
-    text: str, occurrences: list[dict], annotation: dict
+def inject_known_entities(
+    text: str, occurrences: list[dict], known_entities: dict
 ) -> list[str | tuple]:
     """Format text into streamlit annotated text for frontend."""
     cursor = 0
@@ -27,19 +23,24 @@ def inject_annotations(
 
         # Annotate term
         term = text[occurrence["start"] : occurrence["end"]]
-        parsed.append((term, annotation[term], occurrence["link"]))
+        parsed.append((term, known_entities[term], occurrence["link"]))
 
         cursor = occurrence["end"]
     parsed.append(text[cursor:])
     return parsed
 
 
-def st_annotated_block(text: str, annotation: dict = TEST_ANNOTATIONS):
+def st_annotated_block(text: str, known_entities: dict | None = None):
     """Get annotated known entities component for front-end."""
 
-    term_occurrences = find_all_occurrences(text, list(annotation.keys()))
-    formatted = inject_annotations(
-        text=text, occurrences=term_occurrences, annotation=annotation
+    if not known_entities:
+        known_entities = get_known_entities()
+
+    term_occurrences = find_all_occurrences(
+        text=text, words=list(known_entities.keys())
+    )
+    formatted = inject_known_entities(
+        text=text, occurrences=term_occurrences, known_entities=known_entities
     )
 
     block = ""
--- text2graph/macrostrat.py
+++ text2graph/macrostrat.py
@@ -6,7 +6,7 @@ BASE_URL = "https://macrostrat.org/api"
 
 
 @cache
-def get_all_strat_names() -> list:
+def get_all_strat_names() -> list[str]:
     """Get all stratigraphic names from macrostrat API."""
 
     url = f"{BASE_URL}/defs/strat_names?all"
@@ -16,12 +16,53 @@ def get_all_strat_names() -> list:
     return sorted(list(set([x["strat_name"] for x in data])))
 
 
-def get_strat_records(strat_name=str) -> list[dict]:
+@cache
+def get_all_lithologies() -> list[str]:
+    """Get all lithologies from macrostrat API."""
+
+    url = f"{BASE_URL}/defs/lithologies?all"
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()["success"]["data"]
+    return sorted(list(set([x["name"] for x in data])))
+
+
+def get_known_entities() -> dict:
+    """Get known entities for annotations."""
+    lithologies = get_all_lithologies()
+    strat_names = get_all_strat_names()
+    return {
+        **{lith: "lithology" for lith in lithologies},
+        **{strat: "strat name" for strat in strat_names},
+    }
+
+
+@cache
+def get_strat_records(strat_name=str, exact: bool = False) -> list[dict]:
     """Get the records for a given stratigraphic name."""
 
     response = requests.get(f"{BASE_URL}/defs/strat_names?strat_name={strat_name}")
     response.raise_for_status()
-    return response.json()["success"]["data"]
+    matches = response.json()["success"]["data"]
+    if exact:
+        matches = [match for match in matches if match["strat_name"] == strat_name]
+    if not matches:
+        raise ValueError(f"No stratigraphic name found for '{strat_name}'")
+    return matches
+
+
+@cache
+def get_lith_records(lith_name=str, exact: bool = False) -> list[dict]:
+    """Get the records for a given lithology name."""
+
+    response = requests.get(f"{BASE_URL}/defs/lithologies?lith={lith_name}")
+    response.raise_for_status()
+    matches = response.json()["success"]["data"]
+    if exact:
+        matches = [match for match in matches if match["name"] == lith_name]
+    if not matches:
+        raise ValueError(f"No lithology found for '{lith_name}'")
+    return matches
 
 
 def _find_word_occurrences(text: str, search_word: str) -> list[dict]:
