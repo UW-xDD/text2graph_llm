5215bf705d004dc6bcc65b146d392a9657d57e38
ilmcconnell <iain.mcconnell@gmail.com>
2024-03-20 10:53:04 -0500

    gkm conversion with RDFLib

--- text2graph/gkm.py
+++ text2graph/gkm.py
@@ -1,182 +1,224 @@
-import re
-import numpy as np
+import requests
+from enum import IntEnum
+from dataclasses import dataclass
+from rdflib import Graph, Literal, RDF, RDFS, Namespace, URIRef, BNode
+
 from text2graph.schema import RelationshipTriples
 
 
-TripletJson = dict[str, str | dict[str, str]]
-GKMProperties = list[dict[str, str | dict[str, str]]]
-MACROSTRAT_IGNORE_KEYS = [
-    "strat_name_long", "strat_name", "t_units",
-]
-RANK_LOOKUP = {
-    "Mbr": "Member",
-    "Fm": "Formation",
-    "Gp": "Group",
-    "SGp": "Supergroup"
+class Rank(IntEnum):
+    BED = 0
+    MEMBER = 1
+    FORMATION = 2
+    GROUP = 3
+    SUPERGROUP = 4
+
+
+STRAT_RANK_LOOKUP = {
+    'Bed': Rank.BED,
+    'Mbr': Rank.MEMBER,
+    'Fm': Rank.FORMATION,
+    'Gp': Rank.GROUP,
+    'SGp': Rank.SUPERGROUP
 }
 
+GSOC = Namespace("https://w3id.org/gso/1.0/common/")
+GSOG = Namespace("https://w3id.org/gso/geology/")
+GSGU = Namespace("https://w3id.org/gso/geologicunit/")
+GSPR = Namespace("https://w3id.org/gso/geologicprocess/")
+GST = Namespace("https://w3id.org/gso/geologictime/")
+XDD = Namespace("https://w3id.org/xdd-lexicography/")
 
-def triplet_to_gkm(triplet: RelationshipTriples | TripletJson) -> GKMProperties:
-    """
-    Convert a triplet object to a list of GKM properties
-    """
-    try:
-        triplet_json = triplet.dict()
-    except (AttributeError, TypeError):
-        triplet_json = triplet
+RANK_LOOKUP = {
+    'Bed': GSGU.Bed,
+    'Fm': GSGU.Formation,
+    'Mbr': GSGU.Member,
+    'Gp': GSGU.Group,
+    'SGp': GSGU.Supergroup,
+}
 
-    subject_dict = triplet_json['subject']
-    predicate = triplet_json['predicate']
-    object_dict = triplet_json['object']
+BASE_URL = "https://macrostrat.org/api"
 
-    gkm_properties = []
-    subject_name = subject_dict['strat_name_long'].replace(' ', '')
-    gkm_properties.append(
-        {"name": f"xdd:{subject_name}"}
-    )
 
-    for k, v in subject_dict.items():
-        if k == 'rank':
-            try:
-                type_value = RANK_LOOKUP[v]
-            except KeyError:
-                type_value = v
-            gkm_properties.append(
-                {
-                    "rdf:type": "gsgu:" + type_value
-                }
-            )
-            continue
-        elif k[-3:] != "_id" and k not in MACROSTRAT_IGNORE_KEYS and v:
-            gkm_properties.append(
-                {
-                    "gsoc:hasQuality": {
-                        "xdd:" + k: {
-                            "gsoc:hasValue": format_gkm_value(v)
-                        }
-                    }
-                }
-            )
+def get_all_intervals() -> list[dict]:
+    """Get all stratigraphic names from macrostrat API."""
 
-    if predicate == "is_in":
-        gkm_properties.append(
-            {
-                f"xdd:isIn": {
-                    "gsoc:SpatialLocation": {
-                        "gsoc:hasValue": format_gkm_value(object_dict['name'])
-                    }
-                }
-            }
+    url = f"{BASE_URL}/defs/intervals?all"
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()["success"]["data"]
+    return data
+
+
+def create_interval_lookup(intervals: list[dict]) -> dict:
+    lookup = {}
+    for interval in intervals:
+        interval_name = interval["name"].strip().title().replace(" ", "").replace('"', "")
+        interval_type = interval['int_type'].title().replace(" ", "")
+        interval_class_name = interval_name + interval_type
+        gst_class = GST[interval_class_name]
+        lookup[interval_name] = gst_class
+
+    return lookup
+
+
+INTERVAL_LOOKUP = create_interval_lookup(intervals=get_all_intervals())
+
+
+@dataclass
+class RankRelation:
+    name: str
+    rank: Rank
+    rdftype: Namespace
+    entity_name: str = ""
+
+    def __post_init__(self):
+        self.entity_name = self.name.replace(" ", "")
+
+    @classmethod
+    def from_subject_data(cls, subject_data: dict):
+        return cls(
+            name=subject_data["strat_name_long"],
+            rank=STRAT_RANK_LOOKUP[subject_data["rank"]],
+            rdftype=RANK_LOOKUP[subject_data["rank"]]
         )
 
-    gkm_properties.append(
-        {
-            "gsoc:hasQuality": {
-                "gsoc:SpatialLocation": {
-                    "gsoc:hasValue": format_gkm_value(object_dict['name'])
-                }
-            }
-        }
-    )
 
-    spatial_value_str = f"(POINT {format_gkm_value(object_dict['lat'])} {format_gkm_value(object_dict['lon'])})"
-    gkm_properties.append(
-        {
-            "gsoc:hasValue": {
-                "gsoc:SpatialLocation": {
-                    "gsoc:hasValue": {
-                        "gsoc:SpatialValue": spatial_value_str
-                    }
-                }
-            }
-        }
-    )
-    return gkm_properties
-
-
-def format_gkm_value(v: int | float | str) -> str:
+def stratigraphic_rank_relations(g: Graph, subject_data: dict, subject_node: URIRef) -> Graph:
     """
-    return str value with quotes for strs, and not for numbers
-    :param v: object_dict value
-    :return: str value with quotes for strs, and not for numbers
+    Add stratigraphic rank relationships in subject_data to graph
     """
+    subject_rank_relation = RankRelation.from_subject_data(subject_data)
+    for k in STRAT_RANK_LOOKUP.keys():
+        rank_relation_name = subject_data[k.lower()]
+        rank_relation_dct = dict(strat_name_long=rank_relation_name + k, rank=k)
+        if rank_relation_name:
+            relator_rank_relation = RankRelation.from_subject_data(rank_relation_dct)
+            if relator_rank_relation.rank < subject_rank_relation.rank:
+                # print(f"{relator_rank_relation.name} is part of {subject_rank_relation.name}")
+                rank_relation_node = URIRef(relator_rank_relation.entity_name, XDD)
+                g.add((rank_relation_node, RDF.type, relator_rank_relation.rdftype))
+                g.add((rank_relation_node, GSOC.isPartOf, subject_node))
 
-    if isinstance(v, float) or isinstance(v, int):
-        return str(v)
-    if isinstance(v, str) and v.isnumeric():
-        return str(v)
-    else:
-        return '"' + str(v) + '"'
+            if relator_rank_relation.rank > subject_rank_relation.rank:
+                # print(f"{subject_rank_relation.name} is part of {relator_rank_relation.name} ")
+                rank_relation_node = URIRef(relator_rank_relation.entity_name, XDD)
+                g.add((rank_relation_node, RDF.type, relator_rank_relation.rdftype))
+                g.add((subject_node, GSOC.isPartOf, rank_relation_node))
+    return g
 
 
-def gkm_to_string(gkm_properties: GKMProperties, indent: int = 2) -> str:
+def deposition_age(g: Graph, subject_data: dict, subject: URIRef) -> Graph:
     """
-    Convert a list of GKM properties into a formatted string
+    Add subject deposition age to subject in graph
     """
-    end_of_line = "\n"
-    result_str = ""
-    indent_spacer = " " * indent
-    for gkm_entry in gkm_properties:
-        try:
-            result_str += str(gkm_entry["name"]) + end_of_line
-            continue
-        except KeyError:
-            pass
+    period_keys = ["t_period", "b_period"]
+    unique_periods = set([subject_data[k] for k in period_keys])
+    for period in unique_periods:
+        if period:
+            bnode_deposition = BNode()
+            g.add((bnode_deposition, RDF.type, GSPR.Deposition))
+            g.add((bnode_deposition, RDFS.label, Literal(f"Deposition during {period}", lang="en")))
+            g.add((bnode_deposition, GSOC.occupiesTimeDirectly, INTERVAL_LOOKUP[period]))
+            g.add((subject, GSOC.isParticipantIn, bnode_deposition))
 
-        try:
-            property_str = indent_spacer + (nested_dict_to_str(gkm_entry)) + end_of_line
-            result_str += property_str
-        except TypeError as e:
-            print(f"Unnesting error: {gkm_entry}, {e}")
-
-    result_str = square_bracket_indentation(result_str, indent_spacer=indent_spacer)
-    result_str = result_str.replace("]", "] ;")
-    result_str += "\n."
-
-    return result_str
+    return g
 
 
-def nested_dict_to_str(d) -> str:
+def time_span(g: Graph, subject_data: dict, subject: URIRef) -> Graph:
     """
-    Concat a nested dict's keys and values to a string
+    add age range to subject in graph
     """
-    result_str = ""
-    try:
-        for k, v in d.items():
-            if k in ["gsoc:hasQuality", "gsoc:hasValue", "gsoc:SpatialLocation", "gsoc:SpatialValue"]:
-                result_str = k + " [ " + nested_dict_to_str(v) + " ] "
-            else:
-                result_str = k + " " + nested_dict_to_str(v)
+    bnode_interval = BNode()
+    g.add((bnode_interval, RDF.type, GSOG.Geologic_Time_Interval))
+    bnode_interval_location = BNode()
+    g.add((bnode_interval_location, RDF.type, GSOC.Time_Interval_Location))
+    bnode_range = BNode()
+    g.add((bnode_range, RDF.type, GSOC.Temporal_Range))
+    bnode_range_end = BNode()
+    g.add((bnode_range_end, RDF.type, GSOC.Time_Numeric_Value))
+    g.add((bnode_range_end, GSOC.hasDataValue, Literal(float(subject_data['t_age']))))
+    bnode_range_start = BNode()
+    g.add((bnode_range_start, RDF.type, GSOC.Time_Numeric_Value))
+    g.add((bnode_range_start, GSOC.hasDataValue, Literal(float(subject_data['b_age']))))
 
-    except (AttributeError, TypeError):
-        result_str = d + " ;"
-    return result_str
+    g.add((subject, GSOC.occupiesTimeDirectly, bnode_interval))
+    g.add((bnode_interval, GSOC.hasQuality, bnode_interval_location))
+    g.add((bnode_interval_location, GSOC.hasValue, bnode_range))
+    g.add((bnode_range, GSOC.hasEndValue, bnode_range_end))
+    g.add((bnode_range, GSOC.hasStartValue, bnode_range_start))
+    return g
 
 
-def square_bracket_indentation(property_str: str, indent_spacer: str) -> str:
-    indented_str = ""
-    indent_level = 1
-    while len(property_str) > 0:
-        open_bracket_hit = re.search(r"\[", property_str)
-        close_bracket_hit = re.search(r"]", property_str)
-        open_bracket_span = open_bracket_hit.span() if open_bracket_hit else (np.inf, np.inf)
-        close_bracket_span = close_bracket_hit.span() if close_bracket_hit else (np.inf, np.inf)
-        if open_bracket_span == close_bracket_span == (np.inf, np.inf):
-            break
-        if open_bracket_span[0] < close_bracket_span[0]:
-            indented_str += property_str[:open_bracket_span[1]] + "\n"
-            indent_level += 1
-            indent = indent_spacer * indent_level
-            property_str = indent + property_str[open_bracket_span[1]:]
-        else:
-            indented_str += property_str[:close_bracket_span[0]] + "\n"
-            indent_level -= 1
-            indent_level = max(0, indent_level)
-            indent = indent_spacer * indent_level
-            indented_str += indent + property_str[close_bracket_span[0]:close_bracket_span[1]]
-            try:
-                property_str = indent + property_str[close_bracket_span[1] + 1:]
-            except IndexError:
-                property_str = indent + property_str[close_bracket_span[1]:]
+def spatial_location(g: Graph, subject_data: dict, object_data: dict, subject: URIRef) -> Graph:
+    """
+    Add spatial location to subject in graph
+    """
+    WGS84 = URIRef("https://epsg.io/4326")
+    bnode_sl = BNode()
+    g.add((bnode_sl, RDF.type, GSOC.SpatialLocation)),
+    g.add((subject, GSOC.hasQuality, bnode_sl))
+    bnode_slv = BNode()
+    g.add((bnode_slv, RDF.type, GSOC.SpatialValue))
+    g.add((bnode_slv, GSOC.hasDataValue, Literal(object_data['name'], lang="en")))
+    g.add((bnode_sl, GSOC.hasValue, bnode_slv))
+    bnode_slwkt = BNode()
+    g.add((bnode_slwkt, RDF.type, GSOC.WKT_Value))
+    g.add((bnode_slwkt, GSOC.hasDataValue, Literal(f"( POINT {object_data['lon']} {object_data['lat']} )")))
+    g.add((bnode_slwkt, GSOC.hasReferenceSystem, WGS84))
+    g.add((bnode_sl, GSOC.hasValue, bnode_slwkt))
+    g.add((WGS84, RDF.type, GSOC.Geographic_Coordinate_System))
+    return g
 
-    return indented_str
+
+def default_rdf_graph() -> Graph:
+    """
+    Initialize graph with all namespaces bound
+    :return: graph
+    """
+    # https://loop3d.org/GKM/geology.html
+    g = Graph()
+    g.bind("rdf", RDF)
+    g.bind("rdfs", RDFS)
+    g.bind("gsoc", GSOC)
+    g.bind("gsog", GSOG)
+    g.bind("gsgu", GSGU)
+    g.bind("gst", GST)
+    g.bind("gspr", GSPR)
+    g.bind("xdd", XDD)
+    return g
+
+
+def triplet_to_rdf(triplet: dict | RelationshipTriples) -> Graph:
+    """
+    Convert RelationshipTriples object to an RDF graph
+    :param triplet: RelationshipTriples object to convert
+    :return: RDF graph of RealtionshipTriples object
+    """
+    if isinstance(triplet, RelationshipTriples):
+        triplet = triplet.dict()
+
+    subject_data = triplet["subject"]
+    predicate_data = triplet["predicate"]
+    object_data = triplet["object"]
+
+    g = default_rdf_graph()
+    subject_name = subject_data["strat_name_long"].replace(" ", "")
+    subject = URIRef(subject_name, XDD)
+    g.add((subject, RDF.type, RANK_LOOKUP[subject_data["rank"]]))
+    g.add((subject, RDFS.label, Literal(subject_data["strat_name_long"], lang="en")))
+    g = spatial_location(g=g, subject_data=subject_data, object_data=object_data, subject=subject)
+    g = stratigraphic_rank_relations(g=g, subject_data=subject_data, subject_node=subject)
+    g = deposition_age(g=g, subject_data=subject_data, subject=subject)
+    g = time_span(g=g, subject_data=subject_data, subject=subject)
+
+    return g
+
+
+def graph_to_ttl_string(g: Graph) -> str:
+    """
+    serialize graph to RDF Turtle (ttl) string
+    :param g: graph to serialize
+    :return str: serialized graph
+    """
+    return g.serialize(format="turtle")
