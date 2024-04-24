c637fe1c1c3a8955619feea7ccc52a5fc58eb707
Jason Lo <lcmjlo@gmail.com>
2024-04-24 19:04:36 +0000

    disable script mode

--- chtc/job.sh
+++ chtc/job.sh
@@ -6,8 +6,6 @@ echo "GPUs assigned: $CUDA_VISIBLE_DEVICES"
 export HOME=$_CONDOR_SCRATCH_DIR
 
 echo "Copy ollama cache folder from staging to scratch dir"
-
-# Do not use symlink, very slow and unstable.
 cp -r /staging/clo36/.ollama ~/.ollama
 
 echo "Starting ollama..."
@@ -21,4 +19,4 @@ ollama run mixtral "this is a warm up query"
 
 # Run the worker with burst mode
 echo "Running batch..."
-python -m preprocess_extraction_direct.py --job_index $1
+python preprocess_extraction_direct.py $1 $2
