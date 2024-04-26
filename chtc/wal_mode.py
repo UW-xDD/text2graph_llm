1000430f91acee938742c0d96127178bc9a58f40
Jason Lo <lcmjlo@gmail.com>
2024-04-26 13:45:59 +0000

    add checking

--- chtc/wal_mode.py
+++ chtc/wal_mode.py
@@ -3,6 +3,8 @@ import sqlite3
 if __name__ == "__main__":
     db_path = "/staging/clo36/text2graph/preprocess/triplets.db"
     with sqlite3.connect(db_path) as connection:
-        with connection.cursor() as cur:
-            cur.execute("PRAGMA journal_mode=WAL;")
-            connection.commit()
+        connection.execute("PRAGMA journal_mode=WAL;")
+
+        # Check the journal mode
+        mode = connection.execute("PRAGMA journal_mode;").fetchone()
+        print("Journal Mode:", mode[0])
