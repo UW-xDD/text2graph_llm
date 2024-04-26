08107f1e1fb10f0e81224b182b0757ce3350c62e
Jason Lo <lcmjlo@gmail.com>
2024-04-26 13:43:15 +0000

    get rid of dependency in wal_mode

--- chtc/wal_mode.py
+++ chtc/wal_mode.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-from preprocess_extraction_direct import DB_PATH, enable_wal_mode
-
 if __name__ == "__main__":
-    with sqlite3.connect(DB_PATH) as connection:
-        enable_wal_mode(connection)
+    db_path = "/staging/clo36/text2graph/preprocess/triplets.db"
+    with sqlite3.connect(db_path) as connection:
+        with connection.cursor() as cur:
+            cur.execute("PRAGMA journal_mode=WAL;")
+            connection.commit()
