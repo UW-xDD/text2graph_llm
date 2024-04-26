e623414500e3f4363c9e0462e8cef758aa5c3b85
Jason Lo <lcmjlo@gmail.com>
2024-04-26 16:58:22 +0000

    switch sqlite to turso

--- chtc/preprocess_extraction_direct.py
+++ chtc/preprocess_extraction_direct.py
@@ -1,13 +1,11 @@
 import argparse
 import asyncio
 import logging
-import os
 import pickle
-import sqlite3
-from pathlib import Path
 
 import tenacity
 import weaviate
+from db import Triplets, get_all_processed_ids, push
 from dotenv import load_dotenv
 from tqdm import tqdm
 
@@ -17,54 +15,6 @@ from text2graph.llm import ask_llm
 logging.basicConfig(level=logging.ERROR)
 
 load_dotenv()
-
-DB_NAME = "triplets"
-DB_PATH = os.getenv("DB_PATH", "triplets.db")
-
-
-def create_db() -> None:
-    """Create local DB if it doesn't exist."""
-
-    if Path(DB_PATH).exists():
-        logging.debug("Database already exists.")
-        return
-
-    with sqlite3.connect(DB_PATH) as conn:
-        cur = conn.cursor()
-        cur.execute(f"""
-        CREATE TABLE IF NOT EXISTS {DB_NAME} (
-            id CHAR(64) PRIMARY KEY,
-            hashed_text CHAR(64),
-            paper_id CHAR(32),
-            triplets TEXT
-        )
-        """)
-        conn.commit()
-
-        # Switch to WAL mode
-        cur.execute("PRAGMA journal_mode=WAL;")
-        mode = cur.execute("PRAGMA journal_mode;").fetchone()
-        logging.debug(f"Journal Mode: {mode[0]}")
-
-
-def insert_case(
-    connection: sqlite3.Connection,
-    id: str,
-    hashed_text: str,
-    paper_id: str,
-    triplets: str,
-) -> None:
-    """Insert entity into local DB."""
-
-    cur = connection.cursor()
-    cur.execute(
-        f"""
-    INSERT INTO {DB_NAME} (id, hashed_text, paper_id, triplets)
-    VALUES (?, ?, ?, ?)
-    """,
-        (id, hashed_text, paper_id, triplets),
-    )
-    connection.commit()
 
 
 @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_attempt(3))
@@ -76,8 +26,8 @@ async def extract(text: str, doc_id: str) -> str:
 
 
 def process_paragraph(
-    id: str, weaviate_client: weaviate.Client, connection: sqlite3.Connection
-) -> None:
+    id: str, weaviate_client: weaviate.Client
+) -> dict[str, str] | None:
     """Process extraction pipeline for a paragraph."""
 
     paragraph = weaviate_client.data_object.get_by_id(id)
@@ -100,41 +50,13 @@ def process_paragraph(
         return
 
     logging.info(f"Extracted paragraph {id}: {output}")
-    try:
-        insert_case(connection=connection, **output)
-    except Exception as e:
-        logging.error(f"Failed to insert entities: {id} into the database: {e}")
-        return
-
-    logging.info(f"Inserted entities for paragraph {id} into the database.")
-
-
-def get_processed_count(connection: sqlite3.Connection) -> int:
-    """Get the number of cases already processed."""
-    cur = connection.cursor()
-    cur.execute(f"SELECT COUNT(*) FROM {DB_NAME}")
-    result = cur.fetchone()
-    return result[0]
-
-
-def get_all_processed_ids(connection: sqlite3.Connection) -> list[str]:
-    """Get all processed ids from SQLITE."""
-    cur = connection.cursor()
-    cur.execute(f"SELECT id FROM {DB_NAME}")
-    return [row[0] for row in cur.fetchall()]
+    return output
 
 
 def main(job_index: int = 0, batch_size: int = 2000):
     """Main function to process paragraphs."""
 
-    sql_connection = sqlite3.Connection(DB_PATH)
     weaviate_client = get_weaviate_client()
-
-    # Check the journal mode is in WAL
-    sql_connection.execute("PRAGMA journal_mode=WAL;")
-    mode = sql_connection.execute("PRAGMA journal_mode;").fetchone()
-    logging.info(f"SQLite Journal Mode: {mode[0]}")
-    assert mode[0] == "wal"
 
     # Get ids to process
     batch_start_idx = job_index * batch_size
@@ -145,18 +67,21 @@ def main(job_index: int = 0, batch_size: int = 2000):
     batch_ids = all_ids[batch_start_idx : batch_start_idx + batch_size]
 
     ## Remove processed
-    processed = get_all_processed_ids(sql_connection)
+    processed = get_all_processed_ids()
     batch_ids = [id for id in batch_ids if id not in processed]
 
     for id in tqdm(batch_ids):
         try:
-            process_paragraph(id, weaviate_client, sql_connection)
+            out = process_paragraph(id, weaviate_client)
+            if out is None:
+                continue
+            push([Triplets(**out)])
+
         except Exception as e:
             logging.error(f"Failed to process paragraph {id}: {e}")
             continue
 
     logging.info(f"Finished processing batch {job_index=}")
-    sql_connection.close()
 
 
 if __name__ == "__main__":
