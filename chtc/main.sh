3c5eaab677121f016074e1cd0f6efe1ff2465eb8
Jason Lo <lcmjlo@gmail.com>
2024-04-26 19:30:01 +0000

    resume test

--- chtc/main.sh
+++ chtc/main.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-source .env
-
 echo "Running job on `hostname`"
 echo "GPUs assigned: $CUDA_VISIBLE_DEVICES"
 
+echo "Setting up environment variables"
+source .env
 export HOME=$_CONDOR_SCRATCH_DIR
 export PYTHONPATH="$PYTHONPATH:/run"  # Workaround for pip install fails
-export http_proxy=''  # Fix ollama over http issue
+export http_proxy=''  # Fix Ollama over http issue
 
-echo "install extra dependencies"
+echo "installing extra dependencies"
 pip install weaviate-client sqlalchemy-libsql libsql-experimental
 
-echo "Starting ollama..."
+echo "Starting Ollama..."
 ollama serve &
 
-echo "Waiting for ollama to start"
+echo "Waiting for Ollama to start"
 sleep 10
 
-# Warm up
-echo "Warming up ollama"
+echo "Warming up Ollama"
 ollama run mixtral "this is a warm up query"
 
-# Disable model unload from memory
+echo "Disable Ollama model auto-unload from memory"
 curl http://127.0.0.1:11434/api/generate -d "{\"model\": \"mixtral\", \"keep_alive\": -1}"
 
-# Run the worker with burst mode
 echo "Running batch..."
-
-# Fix the python issue
 python preprocess_extraction_direct.py $1 $2
 
 echo "Job completed"
