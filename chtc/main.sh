314678839b151a474fedc41e817f82e1a2dad784
Jason Lo <lcmjlo@gmail.com>
2024-05-03 12:18:35 -0500

    try to fix sqlalchemy problem

--- chtc/main.sh
+++ chtc/main.sh
@@ -9,7 +9,12 @@ export HOME=$_CONDOR_SCRATCH_DIR
 export http_proxy=''
 
 echo "Start running batch..."
+
+# Debug
+pip install SQLAlchemy==2.0.29
+pip show sqlalchemy
 echo "$(pip freeze)"
+
 python3 preprocess_extraction_direct.py --id_pickle geoarchive_paragraph_ids.pkl --job_index $1 --debug
 
 echo "Job completed"
