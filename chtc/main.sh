9402578cb62b6e7c34371af0b59cd4afe97ded91
Jason Lo <lcmjlo@gmail.com>
2024-05-03 11:22:05 -0500

    bug fix: container only has python3

--- chtc/main.sh
+++ chtc/main.sh
@@ -9,6 +9,6 @@ export HOME=$_CONDOR_SCRATCH_DIR
 export http_proxy=''
 
 echo "Start running batch..."
-python preprocess_extraction_direct.py --id_pickle geoarchive_paragraph_ids.pkl --job_index $1 --debug
+python3 preprocess_extraction_direct.py --id_pickle geoarchive_paragraph_ids.pkl --job_index $1 --debug
 
 echo "Job completed"
