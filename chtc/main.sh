1e88210f42c18441473f067256b037a8d505505a
Jason Lo <lcmjlo@gmail.com>
2024-05-03 11:50:39 -0500

    debug versioning problem

--- chtc/main.sh
+++ chtc/main.sh
@@ -9,6 +9,8 @@ export HOME=$_CONDOR_SCRATCH_DIR
 export http_proxy=''
 
 echo "Start running batch..."
+pip freeze
+which python3
 python3 preprocess_extraction_direct.py --id_pickle geoarchive_paragraph_ids.pkl --job_index $1 --debug
 
 echo "Job completed"
