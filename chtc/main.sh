5e7660813e04ea30e0f1667d09d646903e861fc2
Jason Lo <lcmjlo@gmail.com>
2024-05-06 14:16:11 -0500

    test using /bin/bash

--- chtc/main.sh
+++ chtc/main.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export HOME=$_CONDOR_SCRATCH_DIR
 echo "Running job on `hostname`"
@@ -10,11 +10,11 @@ source .env
 pip freeze > freeze.txt
 
 echo "Start running batch..."
-python3
-# python3 -m preprocess_extraction_direct \
-#     --id_pickle geoarchive_paragraph_ids.pkl \
-#     --job_index_start $1 \
-#     --job_index_end $1 \
-#     --debug
+
+python3 -m preprocess_extraction_direct \
+    --id_pickle geoarchive_paragraph_ids.pkl \
+    --job_index_start $1 \
+    --job_index_end $1 \
+    --debug
 
 # echo "Job completed"
