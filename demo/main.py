469c98c508787b1af26818cdae8c699ea10a936c
Jason Lo <lcmjlo@gmail.com>
2024-05-10 18:07:38 +0000

    update demo to use `fast_llm_graph_from_search`

--- demo/main.py
+++ demo/main.py
@@ -3,24 +3,51 @@ import logging
 
 import streamlit as st
 
-from text2graph.llm import llm_graph_from_search
+from text2graph.llm import fast_llm_graph_from_search
 
 logging.basicConfig(level=logging.INFO)
 st.title("Ask-XDD: Location extraction demo")
 
 # Sidebar
 with st.sidebar:
+    st.header("Settings")
     ttl = st.radio("Use TTL format", [True, False])
+    top_k = st.slider(
+        "Top-k",
+        1,
+        100,
+        1,
+        help="Number of paragraphs to retrieve from ask-xDD retriever.",
+    )
+    hydrate = st.radio(
+        "Hydrate entities",
+        [False, True],
+        help="Get entities details from external services, enabling this can be very slow due to rate limit.",
+    )
+    st.header("Note")
     st.markdown(
-        "We are currently processing the LLM call in real-time, so we're using top-k=1 for demonstration purposes. We are improving this by preprocessing the LLM extraction for quicker responses, expected to be available between late May and early June 2024."
+        "We are using the cached LLM graph for faster processing. However, the hydration step (retrieving entity details) is still processed in real time; we are working on caching this step as well."
+    )
+    st.markdown(
+        "*Initial loading may take longer as artifacts are being retrieved. Future runs will be faster."
+    )
+    st.markdown(
+        "For more information, please refer to our [repo](https://github.com/UW-xDD/text2graph_llm#readme)."
     )
 
 # Main content
 query = st.text_input("Query", "Gold mines in Nevada")
-if st.button("Run Models"):
+if st.button("Run"):
     st.markdown("### Run LLM and gather location and Macrostrat data")
+    if hydrate and top_k > 3:
+        st.warning(
+            "Hydrating entities with top-k > 3 can be very slow. Please reduce top-k."
+        )
+        st.stop()
     with st.spinner("Running models..."):
-        outputs = llm_graph_from_search(query=query, top_k=1, model="mixtral", ttl=ttl)
+        outputs = fast_llm_graph_from_search(
+            query=query, top_k=top_k, hydrate=hydrate, ttl=ttl
+        )
         outputs = asyncio.run(outputs)
         logging.info(outputs)
         st.code(outputs)
