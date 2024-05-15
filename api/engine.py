6d273cb997654cf0627f5a47680f71222ff7d71d
Jason Lo <lcmjlo@gmail.com>
2024-05-15 18:00:03 +0000

    fix typing and more explicit alignment handing

--- api/engine.py
+++ api/engine.py
@@ -1,5 +1,6 @@
 import text2graph.llm as llm
-from text2graph.prompt import PromptHandlerV3
+from text2graph.alignment import EntityType, get_alignment_handler
+from text2graph.prompt import StratPromptHandlerV3
 from text2graph.schema import GraphOutput
 
 
@@ -8,20 +9,21 @@ async def llm_graph(text: str, model: str):
 
     return await llm.ask_llm(
         text=text,
-        prompt_handler=PromptHandlerV3(),
+        prompt_handler=StratPromptHandlerV3(),
+        alignment_handler=get_alignment_handler(EntityType.STRAT_NAME),
         model=model,
         temperature=0.0,
         to_triplets=True,
     )
 
 
-async def slow_llm_graph_from_search(**kwargs) -> str | GraphOutput:
+async def slow_llm_graph_from_search(**kwargs) -> list[str] | list[GraphOutput]:
     """Business logic layer for llm graph extraction from search."""
     # Add more API customization logic here if needed.
     return await llm.llm_graph_from_search(model="mixtral", **kwargs)
 
 
-async def fast_llm_graph_from_search(**kwargs) -> str | GraphOutput:
+async def fast_llm_graph_from_search(**kwargs) -> list[str] | list[GraphOutput]:
     """Business logic layer for llm graph extraction from search using cached graph."""
     # Add more API customization logic here if needed.
     return await llm.fast_llm_graph_from_search(**kwargs)
