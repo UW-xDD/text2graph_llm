a1c0457f014b9aa8700f7e9a51d1f24474fc5e32
Jason Lo <lcmjlo@gmail.com>
2024-06-14 20:00:46 +0000

    add some minor turso script

--- chtc/scripts/turso_snippets.sh
+++ chtc/scripts/turso_snippets.sh
@@ -1,5 +1,11 @@
+# Install
+curl -sSfL https://get.tur.so/install.sh | bash
+
+# Login
+turso auth login --headless
+
 # Dump from Turso's database
-turso db shell usgs .dump > ./tmp/dump_240508.sql
+turso db shell usgs-mineral .dump > ./tmp/dump_mineral_240614.sql
 
 # Restore a database from a dump
-sqlite3 ./tmp/data_240508.sqlite3 < ./tmp/dump_240508.sql
+sqlite3 ./tmp/mineral_data_240614.sqlite3 < ./tmp/dump_mineral_240614.sql
